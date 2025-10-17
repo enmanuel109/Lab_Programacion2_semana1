faac6740c9919f3d6af1c137536522c2a1e3cf27
Salcador Cantarero <enmanuelcantarer001@gmial.com>
2025-10-17 10:44:26 -0600

    Funciones ya listas, sin recursividad

--- Lab_Programacion2_semana1/src/lab_programacion2_semana1/EmailAccount.java
+++ Lab_Programacion2_semana1/src/lab_programacion2_semana1/EmailAccount.java
@@ -9,5 +9,57 @@ package lab_programacion2_semana1;
  * @author Cantarero
  */
 public class EmailAccount {
+      String direccionEmail;
+    String password;
+    String nombreUsuario;
+    email[] inbox;
+    int contar = 0;
     
+    public EmailAccount(String direccionEmail, String password, String nombreUsuario) {
+        this.direccionEmail = direccionEmail;
+        this.password = password;
+        this.nombreUsuario = nombreUsuario;
+        this.inbox = new email[10];
+    }
+
+    public String getDireccionEmail() {
+        return direccionEmail;
+    }
+    
+    public String getpassword(){
+        return password;
+    } 
+    
+    public String getnombreUsuario(){
+        return nombreUsuario;
+    } 
+    
+    public boolean recibirEmail(email em){
+        for(int i=0;i<inbox.length;i++){
+            if(inbox[i]==null){
+                inbox[i]=em;
+                return true;
+            }
+        }
+        return false;
+    }
+        
+    public void printInbox(){
+        for(int i =0; i<inbox.length;i++){
+        System.out.println("Posicion:"+inbox[i]+" Emisor:"+ inbox[i].getEmisor()+ " Asunto:"+ inbox[i].getAsunto()+  inbox[i].getLeido());
+            System.out.println("Todos los correos: ");
+            System.out.println("Sin leer: ");
+        }
+    }
+    
+    public void leerEmail(int pos){
+     for(int i=0;i<inbox.length;i++){
+            if(inbox[i]!=null){
+                inbox[i].print();
+               inbox[i].getLeido();
+            }else{
+            System.out.println("Correo no existe.");
+            }
+        }
+    }
 }
