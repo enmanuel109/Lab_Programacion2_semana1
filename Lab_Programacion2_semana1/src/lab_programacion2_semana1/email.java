0cc1f6c8265797eecfa3add24875946a28f53d66
Jiny-perez <jinyperez55@gmail.com>
2025-10-17 08:54:52 -0600

    Atributos y funcionalidad ya realizada.

--- Lab_Programacion2_semana1/src/lab_programacion2_semana1/email.java
+++ Lab_Programacion2_semana1/src/lab_programacion2_semana1/email.java
@@ -1,13 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab_programacion2_semana1;
+
+import java.util.Calendar;
 
 /**
  *
- * @author David
+ * @author Jiny
  */
 public class email {
-    
+
+    private String emisor;
+    private String asunto;
+    private String contenido;
+    private boolean leido;
+    private Calendar fechaEnvio;
+
+    public email(String emisor, String asunto, String contenido) {
+        this.asunto = asunto;
+        this.contenido = contenido;
+        this.emisor = emisor;
+        this.leido = false;
+        this.fechaEnvio = Calendar.getInstance();
+    }
+
+    public String getEmisor() {
+        return emisor;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public boolean getLeido() {
+        return leido;
+    }
+
+    public void leido() {
+        if (!leido) {
+            leido = true;
+            System.out.println("LEIDO");
+        }
+    }
+
+    public Calendar getFechaEnvio() {
+        return fechaEnvio;
+    }
+
+    public void print() {
+        System.out.println("De:  " + emisor);
+        System.out.println("Asunto:  " + asunto);
+        System.out.println("Contenido:  " + contenido);
+        System.out.println("Fecha:  " + fechaEnvio);
+        System.out.println("Estado:  " + leido);
+    }
+
 }
