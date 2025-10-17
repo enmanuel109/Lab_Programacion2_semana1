e1dc29946a863f62e00abebcdadb964a3abb9554
DJCC <david.carcamo2006@gmail.com>
2025-10-17 09:02:22 -0600

    Desarrollo de pestania inicial

---
+++ Lab_Programacion2_semana1/src/lab_programacion2_semana1/menuInicial.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab_programacion2_semana1;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author David
+ */
+public class menuInicial {
+   //Creacion de Jframe
+    
+    private static EmailAccount cuentaLogged;
+    
+    
+    public menuInicial(EmailAccount cuentaLogged){
+        JFrame screen = new JFrame();
+        this.cuentaLogged= cuentaLogged;
+        screen.setSize(1000, 800);  //Tamaño standard para menus
+        screen.setResizable(false);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        screen.setLocationRelativeTo(null);
+        screen.setLayout(null);
+        
+        JLabel titulo= new JLabel("Menu Inicial");
+        titulo.setBounds(300, 20, 500, 200);
+        titulo.setFont(new Font("Serif", Font.BOLD, 80));
+        
+        JButton botonLogin = new JButton("Iniciar Sesion");
+        JButton botonNewAccount = new JButton("Crear Cuenta");
+        JButton botonSalir = new JButton("Salir");
+        
+        botonLogin.setBounds(400, 200, 250, 100);
+        botonNewAccount.setBounds(400, 350, 250, 100);
+        botonSalir.setBounds(400, 500, 250, 100);
+        
+        
+        
+        botonLogin.addActionListener(new ActionListener(){
+          @Override 
+          public void actionPerformed(ActionEvent e){
+              System.out.println("Empieza proceso de Login");
+          }
+                    
+        });
+        
+        botonNewAccount.addActionListener(new ActionListener(){
+          @Override 
+          public void actionPerformed(ActionEvent e){
+              System.out.println("Empieza proceso de Crear cuenta");
+          }
+                    
+        });
+        
+        botonSalir.addActionListener(new ActionListener(){
+          @Override 
+          public void actionPerformed(ActionEvent e){
+              screen.dispose();
+          }
+                    
+        });
+        
+        
+        
+        
+        screen.add(botonLogin);
+        screen.add(botonNewAccount);
+        screen.add(botonSalir);
+        screen.add(titulo);
+        
+        
+        
+        screen.setVisible(true);
+    }
+    
+    
+    public static void main(String[] args) {
+        menuInicial ventana= new menuInicial(cuentaLogged);
+    }
+       
+    
+}
